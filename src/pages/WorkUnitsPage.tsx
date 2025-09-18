5567a255707873750897a0608e25eb1bc0803a4f
Maksym Panov <work.maksym.panov@gmail.com>
2025-09-18 23:01:32 +0300

    Fix table and add bottom part with summary

--- src/pages/WorkUnitsPage.tsx
+++ src/pages/WorkUnitsPage.tsx
@@ -41,7 +41,12 @@ const WorkUnitsPage = () => {
     return (
         <Paper
             className={`${paperStyles.paper} ${commonStyles.flexColumn}`}
-            style={{ gap: theme.spacing(4), borderRadius: '10px', maxHeight: '80vh' }}
+            style={{
+                gap: theme.spacing(4),
+                borderRadius: '10px',
+                paddingBottom: theme.spacing(4),
+                marginBottom: theme.spacing(8),
+            }}
         >
             <Box
                 display="flex"
@@ -95,6 +100,7 @@ const WorkUnitsPage = () => {
             <TableContainer
                 style={{
                     minWidth: '350px',
+                    maxHeight: '60vh',
                     overflow: 'auto',
                     boxSizing: 'content-box',
                 }}
@@ -163,6 +169,128 @@ const WorkUnitsPage = () => {
                     </TableBody>
                 </Table>
             </TableContainer>
+
+            {report && (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        marginTop: theme.spacing(4),
+                    }}
+                    alignSelf="flex-end"
+                >
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" color="text.secondary">
+                                        Усього видано
+                                    </Typography>
+                                </TableCell>
+                                <TableCell
+                                    sx={{ padding: 0, border: 'none', width: '50px' }}
+                                ></TableCell>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" fontWeight={900} textAlign="right">
+                                        {report.totalIssued.toFixed(2)} гр
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" color="text.secondary">
+                                        Усього повернено (без %)
+                                    </Typography>
+                                </TableCell>
+                                <TableCell
+                                    sx={{ padding: 0, border: 'none', width: '50px' }}
+                                ></TableCell>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" fontWeight={900} textAlign="right">
+                                        {report.totalReturned.toFixed(2)} гр
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" color="text.secondary">
+                                        Усього повернено (з %)
+                                    </Typography>
+                                </TableCell>
+                                <TableCell
+                                    sx={{ padding: 0, border: 'none', width: '50px' }}
+                                ></TableCell>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" fontWeight={900} textAlign="right">
+                                        {report.totalReturnedWithLoss.toFixed(2)} гр
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" color="text.secondary">
+                                        Витрачено на замовлення
+                                    </Typography>
+                                </TableCell>
+                                <TableCell
+                                    sx={{ padding: 0, border: 'none', width: '50px' }}
+                                ></TableCell>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" fontWeight={900} textAlign="right">
+                                        {report.savedByEmployee
+                                            ? report.spentOnOrders.toFixed(2)
+                                            : '–'}{' '}
+                                        гр
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" color="text.secondary">
+                                        Втрачено
+                                    </Typography>
+                                </TableCell>
+                                <TableCell
+                                    sx={{ padding: 0, border: 'none', width: '50px' }}
+                                ></TableCell>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography
+                                        variant="body1"
+                                        fontWeight={900}
+                                        textAlign="right"
+                                        color="error"
+                                    >
+                                        {report.lost.toFixed(2)} гр
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography variant="body1" color="text.secondary">
+                                        Повернено
+                                    </Typography>
+                                </TableCell>
+                                <TableCell
+                                    sx={{ padding: 0, border: 'none', width: '50px' }}
+                                ></TableCell>
+                                <TableCell sx={{ padding: 0, border: 'none' }}>
+                                    <Typography
+                                        variant="body1"
+                                        fontWeight={900}
+                                        textAlign="right"
+                                        color="green"
+                                    >
+                                        {report.savedByEmployee
+                                            ? report.savedByEmployee.toFixed(2)
+                                            : '–'}{' '}
+                                        гр
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </Box>
+            )}
         </Paper>
     );
 };
