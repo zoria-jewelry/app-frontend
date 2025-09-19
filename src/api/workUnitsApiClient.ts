e0b0a9f22aafd84bd97bff2196b64ffb847573b4
Maksym Panov <work.maksym.panov@gmail.com>
2025-09-20 01:41:36 +0300

    Finalize api client for work units

--- src/api/workUnitsApiClient.ts
+++ src/api/workUnitsApiClient.ts
@@ -1,6 +1,11 @@
 import { AbstractApiClient } from './abstractApiClient.ts';
 import type { WorkUnitsReportDto } from '../dto/work-units.ts';
 import type { WorkUnitsFilterData } from '../components/modal/work-units/WorkUnitsFilterComponent.tsx';
+import type {
+    CreateWorkUnitFormData,
+    ReturnWorkUnitFormData,
+    SaveMetalFormData,
+} from '../validation/schemas.ts';
 
 export class WorkUnitsApiClient extends AbstractApiClient {
     public static async getReportForPeriod({
@@ -15,5 +20,17 @@ export class WorkUnitsApiClient extends AbstractApiClient {
         );
         const response = await fetch(`/work-units-report.json`);
         return (await response.json()) as unknown as WorkUnitsReportDto;
+    }
+
+    public static async issueMetal(data: CreateWorkUnitFormData): Promise<void> {
+        console.log(`WorkUnitsApiClient.issueMetal: ${data}`);
+    }
+
+    public static async returnMetal(data: ReturnWorkUnitFormData): Promise<void> {
+        console.log(`WorkUnitsApiClient.returnMetal: ${data}`);
+    }
+
+    public static async saveMetal(data: SaveMetalFormData): Promise<void> {
+        console.log(`WorkUnitsApiClient.saveMetal: ${data}`);
     }
 }
